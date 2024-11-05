fc90a57d08763964320ce360bc1649b6112eb5a1
otorquati <torquati.osvaldo@gmail.com>
2024-11-05 18:11:26 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+const LoginPage = () => {
+    return (
+        <div className="grid h-full grid-cols-2">
+            {/* ESQUERDA */}
+            <div className="mx-auto flex flex-col h-full justify-center p-8 max-w-[550px]">
+                <Image src="/logo.svg" width={173} height={39} alt="Finance AI" className="mb-8"/>
+                <h1 className="mb-3 text-4xl font-bold">Bem Vindo</h1>
+                <p className="text-muted-foreground mb-8">
+                    A Finance AI é uma plataforma de gestão financeira que utiliza IA para 
+                    monitorar suas movimentações, e oferecer insights personalizados, 
+                    facilitando o controle do seu orçamento.
+                </p>
+                <Button variant="outline">
+                    <LogInIcon className="mr-2" />
+                    Fazer login ou criar conta
+                </Button>
+            </div>
+            {/* DIREITA */}
+            <div className="relative h-full w-full">
+                <Image src="/login.png" alt="Faça login" fill className="object-cover"/>
+            </div>
+        </div>
+      );
+}
+ 
+export default LoginPage;
--- app/transactions/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-const Transaction = ({ params: { id } }) => {
-  return <h1 className="text-red-500">Transaction: {id}</h1>;
-};
-
-export default Transaction;
