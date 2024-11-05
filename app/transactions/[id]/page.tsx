0261080533f2d4741bcdbbf5c56fb0b4b2e0b6e2
otorquati <torquati.osvaldo@gmail.com>
2024-11-05 17:06:26 -0300

    chore: add tailwind prettier plugin

--- app/transactions/[id]/page.tsx
+++ app/transactions/[id]/page.tsx
@@ -1,5 +1,5 @@
-const Transaction = ({params: {id}}) => {
-    return <h1>Transaction: {id}</h1>;
-}
- 
+const Transaction = ({ params: { id } }) => {
+  return <h1 className="text-red-500">Transaction: {id}</h1>;
+};
+
 export default Transaction;
