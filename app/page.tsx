ea79352192dfec2d91292d38024cb1ccd5773843
otorquati <torquati.osvaldo@gmail.com>
2024-11-05 23:12:49 -0300

    feat: add git hooks procedures

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { UserButton } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
 
 const Home = async () => {
   const { userId } = await auth();
-  if ( !userId ) {
+  if (!userId) {
     redirect("/login");
   }
-  return <div className="flex h-full items-center justify-center">
-    <UserButton showName />
-  </div>
-}
- 
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
+};
+
 export default Home;
